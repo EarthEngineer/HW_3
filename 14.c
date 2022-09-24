3594c6518b70bd570cf232b652172e9c31a51e4b
EarthEngineer <101568875+EarthEngineer@users.noreply.github.com>
2022-09-25 00:32:26 +0300

    Update 14.c

--- 14.c
+++ 14.c
@@ -1,8 +1,7 @@
 #define MAX 10000
 
+void assign(int A[MAX][MAX], int B[MAX][MAX], int n){
 
-void assign(int A[MAX][MAX], int B[MAX][MAX], int n)
-{
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             A[i][j] = B[i][j];
